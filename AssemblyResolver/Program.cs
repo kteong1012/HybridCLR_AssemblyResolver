7e40099da57edf35219d48e1defc63a30d0515a9
lintianbao <lintianbao@gameale.com>
2024-01-09 16:39:24 +0800

    Init repo

---
+++ AssemblyResolver/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace AssemblyResolverNS
+{
+    public class Program
+    {
+        private static string _aotDir;
+        private static string[] _hotFiles;
+
+        public static void Main(string[] args)
+        {
+            /*
+             * help: 
+             * -aot_dir: directory of aot which hotupdate dlls depend on
+             * -hot_files: hotupdate dlls, split by ';'
+             */
+
+            var helpText =
+@"
+help:
+-aot_dir <directory_path> : directory of aot which hotupdate dlls depend on
+-hot_files <dll1_path>;<dll2_path> : hotupdate dlls, split by ';'
+";
+            if (args.Length == 0)
+            {
+                Console.WriteLine(helpText);
+                return;
+            }
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (args[i] == "-aot_dir")
+                {
+                    _aotDir = args[i + 1];
+                }
+                else if (args[i] == "-hot_files")
+                {
+                    _hotFiles = args[i + 1].Split(';');
+                }
+            }
+
+            if (string.IsNullOrEmpty(_aotDir) || _hotFiles == null || _hotFiles.Length == 0)
+            {
+                Console.WriteLine(helpText);
+                return;
+            }
+
+            var resolver = new Resolver(_aotDir, _hotFiles);
+            resolver.Resolve();
+        }
+    }
+}
---
+++ AssemblyResolver/Resolver.cs
@@ -0,0 +1,99 @@
+﻿using dnlib.DotNet;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+
+namespace AssemblyResolverNS
+{
+    public class Resolver
+    {
+        private string _aotDir;
+        private string[] _hotFiles;
+
+        private Dictionary<string, ModuleDefMD> _aotModules;
+
+        public Resolver(string aotDir, string[] hotFiles)
+        {
+            _aotDir = aotDir;
+            _hotFiles = hotFiles;
+        }
+
+        public void Resolve()
+        {
+            Console.WriteLine($"======Starting Resolving assemblies");
+            Console.WriteLine($"AOT dir: {_aotDir}");
+            Console.WriteLine($"Hot files:\n{string.Join("\n", _hotFiles)}");
+
+            var modContext = ModuleDef.CreateModuleContext();
+
+            // 1. Load all dlls in aot dir but ones equal to hot files
+            _aotModules = new Dictionary<string, ModuleDefMD>();
+            var aotDir = new System.IO.DirectoryInfo(_aotDir);
+            var aotFiles = aotDir.GetFiles("*.dll");
+            Console.WriteLine($"——————Loading AOT dlls from '{_aotDir}'");
+            foreach (var aotFile in aotFiles)
+            {
+                if (IsHotFile(aotFile.Name))
+                {
+                    continue;
+                }
+
+                var aotModule = ModuleDefMD.Load(aotFile.FullName, modContext);
+                _aotModules.Add(aotModule.Name, aotModule);
+                Console.WriteLine($"Loaded '{aotModule.Name}'");
+            }
+
+            // 2. Load all hot files and cache them
+            Console.WriteLine($"——————Loading hot files");
+            var hotModules = new System.Collections.Generic.List<ModuleDefMD>();
+            foreach (var hotFile in _hotFiles)
+            {
+                var hotModule = ModuleDefMD.Load(hotFile, modContext);
+                hotModules.Add(hotModule);
+                Console.WriteLine($"Loaded '{hotModule.Name}'");
+            }
+
+            // 3. Resolve all hot modules
+            foreach (var hotModule in hotModules)
+            {
+                var typeRefs = hotModule.GetTypeRefs();
+                Console.WriteLine($"——————Resolving '{hotModule.Name}'");
+                foreach (var typeRef in typeRefs)
+                {
+                    if (_aotModules.TryGetValue(typeRef.DefinitionAssembly.Name + ".dll", out var aotModule))
+                    {
+                        var aotTypeDef = aotModule.Find(typeRef.FullName, false);
+                        if (aotTypeDef != null)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine($"Resolved '{typeRef.FullName}' => {typeRef.DefinitionAssembly.Name}");
+                            Console.ResetColor();
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine($"Failed to resolve '{typeRef.FullName}' => {typeRef.DefinitionAssembly.Name}");
+                            Console.ResetColor();
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine($"No need to resolve '{typeRef.FullName}' => {typeRef.DefinitionAssembly.Name}");
+                    }
+                }
+            }
+
+            Console.WriteLine($"======Finished Resolving assemblies");
+        }
+
+        private bool IsHotFile(string fileName)
+        {
+            foreach (var hotFile in _hotFiles)
+            {
+                if (Path.GetFileName(hotFile) == fileName)
+                    return true;
+            }
+
+            return false;
+        }
+    }
+}
